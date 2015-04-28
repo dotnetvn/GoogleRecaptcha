2a47ab70588a673749d0ac22e7e0bd01ee5e64a8
Phat Ly <phat.ly.27@gmail.com>
2015-04-28 18:16:34 +0700

    Release the 1.2.1 version.

--- GoogleRecaptcha/RecaptchaV2Data.cs
+++ GoogleRecaptcha/RecaptchaV2Data.cs
@@ -8,6 +8,16 @@
 	public class RecaptchaV2Data
 	{
 		/// <summary>
+		/// Gets or sets the _response field. This field indicates that this is the private response returned from the server.
+		/// </summary>
+		private string _response;
+
+		/// <summary>
+		/// Gets or sets the _remoteIp field. This field indicates that this is the this is private remote ip address from the client.
+		/// </summary>
+		private string _remoteIp;
+
+		/// <summary>
 		/// Gets or sets the Secret property. This property indicates that this is the secret key to be set up on the Google Recaptcha server.
 		/// </summary>
 		public string Secret { get; set; }
@@ -17,7 +27,18 @@
 		/// </summary>
 		public string Response
 		{
-			get { return HttpContext.Current.Request.Form["g-recaptcha-response"]; }
+			get 
+			{
+				if (_response == null)
+				{
+					return HttpContext.Current.Request.Form["g-recaptcha-response"];
+				}
+				return _response;
+			}
+			set
+			{
+				_response = value;
+			}
 		}
 
 		/// <summary>
@@ -25,7 +46,18 @@
 		/// </summary>
 		public string RemoteIp
 		{
-			get { return HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"]; }
+			get 
+			{
+				if (_remoteIp == null)
+				{
+					return HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+				}
+				return _remoteIp;
+			}
+			set
+			{
+				_remoteIp = value;
+			}
 		}
 	}
 }
